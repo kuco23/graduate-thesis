80d6f8ecd058b69a9345569fad3a0bb77a870f4c
kuco23 <nseverkar@gmail.com>
2019-10-18 18:24:54 +0200

    julya_polynomial declassed

--- julia_polynomial.py
+++ julia_polynomial.py
@@ -2,77 +2,53 @@ from functools import reduce
 from os import mkdir
 from os.path import isdir
 
-class PolynomialJuliaImage:
+def drawJuliaPolynomialPPM_escapetime(
+    coefs, dims, rangex, rangey, color, file
+):
+    iterlim = 50
+    lambd = 1.0001
+    n = len(coefs) - 1
+    an = abs(coefs[0])
+    C = sum(map(abs, coefs)) - an
+    eps = max(1, 2 * C / 2, pow(2 * lambd / an, 1 / (n-1)))
 
-    def __init__(
-        self, koefs,
-        dims, rangex, rangey,
-        color
-    ):
-        self.coefs = coefs
-        self.deg = len(coefs) - 1
-        
-        self.dims = dims
-        self.rangex = rangex
-        self.rangey = rangey
-        self.color = color
+    def _horner(z):
+        return reduce(lambda x, y: z * x + y, coefs)
 
-        self._lim = 50
-        self._lmbd = 1.00001
-
-        an = abs(coefs[0])
-        C = sum(map(abs, coefs)) - an
-        lmbdeps = pow(2 * self._lmbd / an, 1 / (self.deg-1))
-        self._eps = max(1, 2 * C / 2, lmbdeps)
-
-    def _horner(self, z):
-        return reduce(lambda x, y: z * x + y, self.coefs)
-
-    def _converganceIterations(self, z):
+    def _converganceIterations(z):
         count = 0
-        while count < self._lim and abs(z) <= self._eps:
-            z = self._horner(z)
+        while count < iterlim and abs(z) <= eps:
+            z = _horner(z)
             count += 1
         return count
 
-    def _converganceRGB(self, count):
-        if count == self._lim: return (0,0,0)
-        base = (255 * count) // self._lim
+    def _converganceRGB(count):
+        if count == iterlim: return (0,0,0)
+        base = (255 * count) // iterlim
         return tuple(map(
             lambda c: int(c / 255 * base),
-            self.color
+            color
         ))
 
-    def _coordsToPoint(self, i, j):
-        (a, b), (c, d) = self.rangex, self.rangey
-        x = i / self.dims * (b - a) + a
-        y = j / self.dims * (d - c) + c
+    def _coordsToPoint(i, j):
+        (a, b), (c, d) = rangex, rangey
+        x = i / dims * (b - a) + a
+        y = j / dims * (d - c) + c
         return complex(x, y)
 
-    def _writeHeaderPPM(self, file):
-        dim = self.dims
-        ppmHeader = ('P3\n', f'{dim} {dim}\n', '255\n')
-        for h in ppmHeader: file.write(h)
+    ppmHeader = ('P3\n', f'{dims} {dims}\n', '255\n')
+    for h in ppmHeader: file.write(h)
 
-    def _drawPPM_escapetime(self, file):
-        self._writeHeaderPPM(file)
-        for j in range(self.dims):
-            file.write('\n')
-            for i in range(self.dims):
-                point = self._coordsToPoint(i, j)
-                count = self._converganceIterations(point)
-                file.write('{0} {1} {2}  '.format(
-                    *self._converganceRGB(count)
-                ))
+    for j in range(dims):
+        file.write('\n')
+        for i in range(dims):
+            point = _coordsToPoint(i, j)
+            count = _converganceIterations(point)
+            file.write('{0} {1} {2}  '.format(
+                *_converganceRGB(count)
+            ))
 
-    def _drawPPM_backwardsiter(self, file):
-        self._writeHeaderPPM(file)
-        ...
 
-    def drawPPM(self, file):
-        self._drawPPM_escapetime(file)
-        
-                
 if __name__ == '__main__':
 
     from argparse import ArgumentParser
@@ -106,16 +82,13 @@ if __name__ == '__main__':
         default = [255, 0, 0]
     )
     vals = args.parse_args()
-
     coefs = list(map(complex, vals.coefs.split()))
-    image = PolynomialJuliaImage(
-        coefs, vals.d,
-        vals.re, vals.im,
-        vals.rgb
-    )
-
+    
     if not isdir('images'): mkdir('images')
     filename = f'images/{vals.nm}.ppm'
     open(filename, 'a').close()
     with open(filename, 'w', encoding='ascii') as file:
-        image.drawPPM(file)
+        drawJuliaPolynomialPPM_escapetime(
+            coefs, vals.d, vals.re, vals.im,
+            vals.rgb, file
+        )
