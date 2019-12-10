9b74f183529923f0a8932e591187b5b6a02854fa
kuco23 <nseverkar@gmail.com>
2019-12-10 01:31:26 +0100

    sh add

---
+++ julia_video.sh
@@ -0,0 +1,16 @@
+name="julia_series"
+folder_name="images"
+fps=20
+
+mkdir -p $folder_name &&
+rm -f $folder_name/*.ppm &&
+echo "Compiling the code ..." &&
+g++ julia.cpp lib/julia_polynomial_series.cpp -o julia &&
+echo "Finished Compiling" &&
+echo "Creating ppm files ..." &&
+./julia.exe &&
+echo "Finished making ppm files" &&
+rm -f julia.exe &&
+echo "Making video ..." &&
+python ppms_to_avi.py $name $fps&&
+echo "Finished video"
