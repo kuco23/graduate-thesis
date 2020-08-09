e94d1a7b0409aa03a844e14539a6ab4967542739
kuco23 <nseverkar@gmail.com>
2020-08-09 21:12:07 +0200

    DEM in python

--- pytools/julia.py
+++ pytools/julia.py
@@ -1,58 +1,109 @@
-from functools import reduce
-from os import mkdir
-from os.path import isdir
+"""
+draw one instance of the Julia set for a specific
+polynomial, with custom parameters
+"""
 
-def drawJuliaPolynomialPPM_escapetime(
-    coefs, dims, range_re, range_im, color, file
+from math import log
+from cmath import sqrt
+from functools import reduce
+from matplotlib import cm
+from matplotlib.colors import Normalize
+import numpy as np
+
+overflow = 10**10
+quadabs = lambda z: z.real * z.real + z.imag * z.imag
+
+def drawJuliaPolynomialPPM(
+    ppm, poly, center, radius, alg, px, cmap, order
 ):
-    iterlim = 50
+    iterlim = 400
     L = 1.0001
     
-    n = len(coefs) - 1
-    an = abs(coefs[0])
-    C = sum(map(abs, coefs)) - an
+    n = len(poly) - 1
+    an = abs(poly[0])
+    C = sum(map(abs, poly)) - an
     eps = max(1, 2 * C / 2, pow(2 * L / an, 1 / (n-1)))
+    eps_quad = pow(eps, 2)
 
-    if not (range_re and range_im):
-        range_re, range_im = (-eps, eps), (-eps, eps)
-    (ReS, ReT), (ImS, ImT) = range_re, range_im
+    if not radius: radius = eps
+    cx, cy = center
+    ReS, ReT = cx - radius, cx + radius
+    ImS, ImT = cy - radius, cy + radius
 
-    def horner(z):
-        return reduce(lambda x, y: z * x + y, coefs)
+    ppm.writelines(['P3\n', f'{px} {px}\n', '255\n'])
+    colormap = cm.get_cmap(cmap)
+    if order == 'reversed': colormap = colormap.reversed()
 
-    def converganceIterations(z):
-        count = 0
-        while count < iterlim and abs(z) <= eps:
-            z = horner(z)
-            count += 1
-        return count
+    def horner(p, z):
+        return reduce(lambda x, y: z * x + y, p)
 
-    def converganceRGB(count):
-        if count == iterlim: return (0,0,0)
-        base = (255 * count) // iterlim
-        return tuple(map(
-            lambda c: int(c / 255 * base),
-            color
-        ))
+    def pointGenerator():
+        dim = dre = 2 * radius / px
+        dz = complex(dre, 0)
+        zk = complex(ReS, ImS)
+        for i in range(px):
+            for j in range(px):
+                zk += dz
+                yield zk
+            zk = complex(ReS, zk.imag + dim)
 
-    def coordsToPoint(i, j):
-        x = i / dims * (ReT - ReS) + ReS
-        y = j / dims * (ImT - ImS) + ImS
-        return complex(x, y)
+    def escapetime():
+        gradient = [
+            ' '.join(map(str, map(round, rgba[:3])))
+            for rgba in 255 * colormap(np.linspace(0, 1, iterlim))
+        ] + ['0 0 0']
+        
+        def escapetimeCount(z):
+            zk, count = z, 0
+            while count < iterlim and quadabs(zk) <= eps_quad:
+                zk = horner(poly, zk)
+                count += 1
+            return count
 
-    ppmHeader = ('P3\n', f'{dims} {dims}\n', '255\n')
-    for h in ppmHeader: file.write(h)
+        points = pointGenerator()
+        for i in range(px):
+            ppm.write('\n')
+            for j in range(px):
+                count = escapetimeCount(next(points))
+                ppm.write(gradient[count] + '  ')
 
-    for j in range(dims):
-        file.write('\n')
-        for i in range(dims):
-            point = coordsToPoint(i, j)
-            count = converganceIterations(point)
-            file.write('{0} {1} {2}  '.format(
-                *converganceRGB(count)
-            ))
+    def dem():
+        pixels = np.empty((px, px), dtype=float)
+        dpdz = [(n - i) * an for (i, an) in enumerate(poly[:-1])]
+            
+        def demCount(z):
+            zk, dk = z, 1
+            for _ in range(iterlim):
+                if max(
+                    abs(zk.real) + abs(zk.imag),
+                    abs(dk.real) + abs(dk.imag)
+                ) > overflow: break
+                dk = horner(dpdz, zk) * dk
+                zk = horner(poly, zk)
+            abszk = abs(zk)
+            if abszk < eps: return 0
+            else:
+                estimate = log(abszk, 2) * abszk / abs(dk)
+                return -log(estimate)
 
+        points = pointGenerator()
+        for j in range(px):
+            for i in range(px):
+                pixels[i][j] = demCount(next(points))
 
+        M = pixels.max()
+        pixels[pixels == 0] = M
+        norm = Normalize(pixels.min(), M)
+        gradient = colormap(norm(pixels))
+        for j in range(px):
+            ppm.write('\n')
+            for i in range(px):
+                rgb = map(round, 255 * gradient[i][j][:3])
+                ppm.write(' '.join(map(str, rgb)) + '  ')
+
+    locals()[alg]()
+
+            
 if __name__ == '__main__':
 
     from argparse import ArgumentParser
@@ -63,36 +114,48 @@ if __name__ == '__main__':
         type = str
     )
     args.add_argument(
-        '-d', metavar = 'image dimensions',
-        type = int, default = 800
+        '-c', metavar='center point',
+        type=float, nargs=2,
+        default=(0, 0)
     )
     args.add_argument(
-        '-re', metavar = 'real axis range',
-        type = int, nargs = 2,
-        default = None
+        '-r', metavar='radius',
+        type=float, default=None,
     )
     args.add_argument(
-        '-im', metavar = 'imaginary axis range',
-        type = int, nargs = 2,
-        default = None
+        '-px', metavar='n x n pixels',
+        type=int, default=1000,
+        help='image pixels'
     )
     args.add_argument(
-        '-nm', metavar = 'file name',
-        type = str, default = 'julia'
+        '-alg', metavar='algorithm',
+        type=str, default='escapetime',
+        choices = ['escapetime', 'dem'],
+        help='algorithm name'
     )
     args.add_argument(
-        '-rgb', metavar = 'image color',
-        type = int, nargs = 3,
-        default = [255, 0, 0]
+        '-cm', metavar='colormap',
+        type=str, default='viridis',
+        help='color mapping'
+    )
+    args.add_argument(
+        '-cmo', metavar='colormap order',
+        type=str, default='reversed',
+        choices = ['normal', 'reversed'],
+        help='color mapping order'
+    )
+    args.add_argument(
+        '-fn', metavar='file name',
+        type=str, default='julia',
+        help='file name'
     )
     vals = args.parse_args()
     coefs = list(map(complex, vals.coefs.split()))
     
-    if not isdir('images'): mkdir('images')
-    filename = f'images/{vals.nm}.ppm'
+    filename = vals.fn + '.ppm'
     open(filename, 'a').close()
-    with open(filename, 'w', encoding='ascii') as file:
-        drawJuliaPolynomialPPM_escapetime(
-            coefs, vals.d, vals.re, vals.im,
-            vals.rgb, file
+    with open(filename, 'w', encoding='utf-8') as ppm:
+        drawJuliaPolynomialPPM(
+            ppm, coefs, vals.c, vals.r, vals.alg,
+            vals.px, vals.cm, vals.cmo
         )
--- pytools/mandelbrot.py
+++ pytools/mandelbrot.py
@@ -1,55 +1,102 @@
 """
-draw one instance of the mandelbrot 
-set with custom parameters
+draw one instance of the mandelbrot set,
+with custom parameters
 """
 
+from math import log
 from cmath import sqrt
+from matplotlib import cm
+from matplotlib.colors import Normalize
+import numpy as np
 
-iterlim = 100
-iterfun = lambda z, c: z * z + c
+overflow = 10**10
+quadabs = lambda z: z.real * z.real + z.imag * z.imag
 
-def drawMandelbrotPPM(file, px, re, im, col):
-    (r1, r2), (i1, i2) = re, im
+def drawMandelbrotPPM(
+    ppm, center, radius, alg, px, cmap, order
+):
+    iterlim = 600
+    cx, cy = center
+    ReS, ReT = cx - radius, cx + radius
+    ImS, ImT = cy - radius, cy + radius
 
-    def setPoint(i, j):
-        x = i / px * (r2 - r1) + r1
-        y = j / px * (i2 - i1) + i1
-        return complex(x, y)
+    ppm.writelines(['P3\n', f'{px} {px}\n', '255\n'])
+    colormap = cm.get_cmap(cmap)
+    if order == 'reversed': colormap = colormap.reversed()
 
-    def pointConvergance(point):
-        eps = max(2, abs(point))
-        z, count = complex(0, 0), 0
-        while count < iterlim and abs(z) <= eps:
-            z = iterfun(z, point)
-            count += 1
-        return count
-
+    def pointGenerator():
+        dim = dre = 2 * radius / px
+        dz = complex(dre, 0)
+        zk = complex(ReS, ImS)
+        for i in range(px):
+            for j in range(px):
+                zk += dz
+                yield zk
+            zk = complex(ReS, zk.imag + dim)
+            
     def inCardioidCircle(point):
+        t = sqrt(1 - 4 * point)
         return (
-            abs(1 - sqrt(1 - 4 * point)) < 1 or
-            abs(1 + sqrt(1 + 4 * point)) < 1 or
+            abs(1 - t) < 1 or
+            abs(1 + t) < 1 or
             abs(1 + point) < 0.25
         )
-    
-    def getGradientBase(count):
-        if count == iterlim:
-            return 0
-        return (255 * count) // iterlim
 
-    ppm_header = ['P3\n', f'{px} {px}\n', '255\n']
-    file.writelines(ppm_header)
+    def escapetime():
+        gradient = [
+            ' '.join(map(str, map(round, rgba[:3])))
+            for rgba in 255 * colormap(np.linspace(0, 1, iterlim))
+        ] + ['0 0 0']
         
-    for j in range(px):
-        file.write('\n')
+        def escapetimeCount(c):
+            ck, count = complex(0, 0), 0
+            while count < iterlim and quadabs(ck) <= 4:
+                ck = ck * ck + c
+                count += 1
+            return count
+
+        points = pointGenerator()
         for i in range(px):
-            point = setPoint(i, j)
-            count = iterlim
-            if not inCardioidCircle(point):
-                count = pointConvergance(point)
-            base = getGradientBase(count)
-            r, g, b = map(lambda c: (c / 255) * base, col)
-            file.write(f'{r} {g} {b}  ')
+            ppm.write('\n')
+            for j in range(px):
+                count = escapetimeCount(next(points))
+                ppm.write(gradient[count] + '  ')
+
+    def dem():
+        pixels = np.empty((px, px), dtype=float)
             
+        def demCount(c):
+            ck, dk = c, 1
+            for _ in range(iterlim):
+                if max(
+                    abs(ck.real) + abs(ck.imag),
+                    abs(dk.real) + abs(dk.imag)
+                ) > overflow: break
+                dk = 2 * ck * dk + 1
+                ck = ck * ck + c
+            absck = abs(ck)
+            if absck <= 2: return 0
+            else:
+                estimate = log(absck, 2) * absck / abs(dk)
+                return -log(estimate)
+
+        points = pointGenerator()
+        for j in range(px):
+            for i in range(px):
+                pixels[i][j] = demCount(next(points))
+
+        M = pixels.max()
+        pixels[pixels == 0] = M
+        norm = Normalize(pixels.min(), M)
+        gradient = colormap(norm(pixels))
+        for j in range(px):
+            ppm.write('\n')
+            for i in range(px):
+                rgb = map(round, 255 * gradient[i][j][:3])
+                ppm.write(' '.join(map(str, rgb)) + '  ')
+
+    locals()[alg]()
+        
 
 if __name__ == '__main__':
 
@@ -57,33 +104,47 @@ if __name__ == '__main__':
 
     args = ArgumentParser()
     args.add_argument(
-        '-re', metavar='real axis range',
+        '-c', metavar='center point',
         type=float, nargs=2,
-        default=(-2, 2)
+        default=(0, 0)
     )
     args.add_argument(
-        '-im', metavar='imaginary axis range',
-        type=float, nargs=2,
-        default=(-2, 2)
-    )
-    args.add_argument(
-        '-rgb', metavar='color gradient',
-        type=float, nargs=3,
-        default=(256, 0, 0)
-    )
-    args.add_argument(
-        '-fn', metavar='file name',
-        type=str, default='mandelbrot'
+        '-r', metavar='radius',
+        type=float, default=2,
     )
     args.add_argument(
         '-px', metavar='n x n pixels',
-        type=int, default=1000
+        type=int, default=1000,
+        help='image pixels'
+    )
+    args.add_argument(
+        '-alg', metavar='algorithm',
+        type=str, default='escapetime',
+        choices = ['escapetime', 'dem'],
+        help='algorithm name'
+    )
+    args.add_argument(
+        '-cm', metavar='colormap',
+        type=str, default='viridis',
+        help='color mapping'
+    )
+    args.add_argument(
+        '-cmo', metavar='colormap order',
+        type=str, default='reversed',
+        choices = ['normal', 'reversed'],
+        help='color mapping order'
+    )
+    args.add_argument(
+        '-fn', metavar='file name',
+        type=str, default='mandelbrot',
+        help='file name'
     )
     vals = args.parse_args()
 
     ppm_file = vals.fn + '.ppm'
     open(ppm_file, 'a').close()
-    with open(ppm_file, 'w', encoding='ascii') as f:
+    with open(ppm_file, 'w', encoding='ascii') as ppm:
         drawMandelbrotPPM(
-            f, vals.px, vals.re, vals.im, vals.rgb
+            ppm, vals.c, vals.r, vals.alg,
+            vals.px, vals.cm, vals.cmo
         )
